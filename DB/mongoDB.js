c01e48e61140df612cbed3f4f08b36c27c9aa1e2
Moti gadian <motiga14@gmail.com>
2016-11-28 20:28:47 +0200

    auth

--- DB/mongoDB.js
+++ DB/mongoDB.js
@@ -6,7 +6,7 @@ const DB = require('mongodb').MongoClient;
 var fs = require('fs');
 
 
-var url = 'mongodb://motkeg:SqFkliuhut3KEBve@172.30.103.123:27017/nodejs-db';
+var url = 'mongodb://admin:CHu4gx7mJwGPVKhg@172.30.103.123:27017/nodejs-db';
 
 
 //connect to DB with no Auth
@@ -19,7 +19,7 @@ DB.connect(url,(err,db) =>{
         console.log("Connect succuss to: "+ url);
         var collection = db.collection('users');
 
-        fs.readFile('./users.json', 'utf8',  (err, data) => {
+        fs.readFile('users.json', 'utf8',  (err, data) => {
             if (err)  console.log(err);
             else {
                 console.log('read from file .json Success');
