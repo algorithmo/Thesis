261fa41ae5e44ec541b974608cb893a393250d3f
Moti gadian <motiga14@gmail.com>
2016-11-29 02:02:56 +0200

    create MongoDB class

---
+++ DB/database.js
@@ -0,0 +1,127 @@
+'use strict';
+
+const DB = require('mongodb').MongoClient;
+
+
+class  MongoDB
+{
+
+    constructor(url)
+      {
+          this.url = url;
+          this.DB = DB;
+      }
+
+   Select(dbname,criterie = {})
+   {
+
+     if(typeof criterie !== 'object'  )
+     {
+       console.log("the criterie needed to be  object type" );
+       return false;
+     }
+     DB.connect(this.url,function(err,db){
+       if(err)
+         {
+           console.log("ERROR: " + err);
+           return false;
+         }
+       else
+         {
+            console.log("SUCCEESS to connect " );
+              //here the main action
+
+                    var collection = db.collection(dbname);
+                    collection.find(criterie).toArray( (err, res) => {
+
+                            if (err) console.log(err);
+                            else if(res.length){
+                              console.log(res);
+                            }else{
+                                console.log("NO match found..");
+                            }
+                              db.close();
+
+                         });
+
+               //end action
+               console.log("Action Complate (find) ");
+               return true;
+           }
+        });
+   }
+
+
+    Insert(dbname,data)
+      {
+
+          DB.connect(this.url,function(err,db){
+            if(err)
+              {
+                console.log("ERROR: " + err);
+                return false;
+              }
+            else
+              {
+                 console.log("SUCCEESS to connect " );
+                   //here the main action
+
+                         var collection = db.collection(dbname);
+                         collection.insert(data, (err, res) => {
+
+                                 if (err) console.log(err);
+                                 else {
+                                   console.log('%d records inserted', res.insertedCount);
+                                   db.close();
+                                 }
+                              });
+
+                    //end action
+                    console.log("Action Complate (Insert) ");
+                    return true;
+                }
+             });
+       }
+
+       Update(dbname,criterie,action)
+       {
+         if(typeof criterie !== 'object'  || typeof action !== 'object' )
+         {
+           console.log("the criterie and action needed to be  object type" );
+           return false;
+         }
+         DB.connect(this.url,function(err,db){
+           if(err)
+             {
+               console.log("ERROR: " + err);
+               return false;
+             }
+           else
+             {
+                console.log("SUCCEESS to connect " );
+                  //here the main action
+
+                        var collection = db.collection(dbname);
+                        collection.update(criterie,{$set:action}, (err, res) => {
+
+                                if (err) console.log(err);
+                                else {
+                                  console.log('%d records updated', res.insertedCount);
+                                  db.close();
+                                }
+                             });
+
+                   //end action
+                   console.log("Action Complate (Update) ");
+                   return true;
+               }
+            });
+       }
+
+
+}
+
+
+/*var url = 'mongodb://localhost:27017/';
+const database = new MongoDB(url);*/
+exports.db  = MongoDB;
---
+++ DB/test.js
@@ -0,0 +1,53 @@
+
+var unamepass  = process.env.MONGODB_USER +':'+ process.env.MONGODB_PASSWORD + '@';
+var url        = 'mongodb://'+unamepass+'172.30.103.123:27017/nodejs-db';
+
+//creating instance of MongoDB class
+var mongoClass = require('./database').db;
+var mongodb    = new  mongoClass(url);
+
+const fs = require('fs');
+
+
+var data = [
+  {
+     id:11111,
+     name:'moti',
+     last:'gad',
+     rating:7.2,
+     skills: { js:3.3 ,html:5.0,css:6,py:8}
+
+  },
+  {
+     id:11112,
+     name:'sasson',
+     last:'jibli',
+     rating:4.2,
+     skills: { js:3.3 ,html:5.0,css:6}
+
+  },
+  {
+     id:11113,
+     name:'matan',
+     last:'avra',
+     rating:1.2,
+     skills: { rb:3.4 ,html:5.0,css:6,py:8,java:8.0}
+
+  }
+]
+
+
+
+
+
+/*fs.readFile('./users.json' , 'utf8' ,(err,data) =>{
+  if(err) console.log(err);
+  else{
+    data = JSON.parse(data);
+    mongodb.Insert('users2',data);
+
+  }
+});*/
+
+mongodb.Insert('users2',data);
+mongodb.Select('users',{rating: {$gt: 4.0}});
